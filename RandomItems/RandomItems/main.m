6d05c2b5b81dea4f2284955338190bf5a50f2d73
Axel Han <axknightroad@gmail.com>
2015-12-10 13:50:09 +0800

    test

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Axel Han on 15/12/9.
+//  Copyright © 2015年 Axel Han. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
